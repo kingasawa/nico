3a6ca90257c2f78ac5a437aed9f48830e0a52b12
khanhtran <khanh@syncfab.com>
2018-12-03 10:43:54 +0700

    update db name

--- config/custom.js
+++ config/custom.js
@@ -2,7 +2,7 @@
 module.exports.custom = {
   'baseUrl':'beta.vnmagic.net',
   'shopifyApiKey': 'c96a3a699140445f31083d020d141a0e',
-  'shopifySecretKey': 'fa30e6fe971a6d900e585764b2878ae3',
+  'shopifySecret': 'fa30e6fe971a6d900e585764b2878ae3',
   'shopifyScope': 'write_price_rules,write_products,read_orders,' +
                   'read_customers,write_customers,read_orders,write_orders,' +
                   'read_shipping,write_shipping,read_analytics',
