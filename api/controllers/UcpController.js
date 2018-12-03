1dd93631db4c0bde3bbaa299b20312671c9c245c
khanhtran <khanh@syncfab.com>
2018-12-03 11:23:02 +0700

    update db name

--- api/controllers/UcpController.js
+++ api/controllers/UcpController.js
@@ -12,7 +12,7 @@ module.exports = {
   shop: async (req, res) => {
     let {id} = req.user
     console.log('req.user', req.user);
-    let shops = await Shop.find({owner:id})
+    let shops = await Shop.find({owner:id}).populate('owner')
     console.log('shops', shops);
     return res.view('pages/ucp/shop',{shops})
   },
@@ -20,7 +20,7 @@ module.exports = {
   product: async (req, res) => {
     let {id} = req.user
     console.log('req.user', req.user);
-    let products = await Product.find({owner:id})
+    let products = await Product.find({owner:id}).populate('owner')
     let shops = await Shop.find({owner:id})
 
     console.log('products', products);
